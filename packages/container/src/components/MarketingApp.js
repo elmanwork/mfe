426cfc9f510f025e78335b1b5473c80e27f432a8
Elman Astanov <e.astanov@asan.local>
2022-04-22 07:40:08 +0400

    container was updated

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -4,7 +4,7 @@ import React, {useRef, useEffect} from 'react';
 export default () => {
     const ref  = useRef(null);
     useEffect(() => {
-        mount(ref.current )
+        mount(ref.current)
     }, [])
 
     return <div ref = {ref} />
